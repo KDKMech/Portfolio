44fe81a4bd8a7597bf1664c7035d8be3ae27f6a2
milestarricone <milestarricone2@gmail.com>
2024-12-02 19:15:35 -0500

    buttons of skills made. focus on dropdowns

---
+++ src/components/skills.jsx
@@ -0,0 +1,42 @@
+
+
+const mySkills = [
+    {
+      name: `JavaScript`,
+      skillLevel: 8,
+      frontEnd: true,
+      backEnd: true
+    },
+    {
+      name: `HTML`,
+      skillLevel: 10,
+      frontEnd: true,
+      backEnd: false
+    },
+    {
+      name: `CSS`,
+      skillLevel: 10,
+      frontEnd: true,
+      backEnd: false
+    },
+    {
+      name: `Express`,
+      skillLevel: 7,
+      frontEnd: true,
+      backEnd: true
+    },
+    {
+      name: `MongoDB`,
+      skillLevel: 7,
+      frontEnd: false,
+      backEnd: true
+    },
+    {
+      name: `EJS`,
+      skillLevel: 8,
+      frontEnd: true,
+      backEnd: true
+    },
+  ]
+
+export default mySkills
